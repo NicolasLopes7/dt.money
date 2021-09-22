4ff3c64cd3a27c14e13bede46e74ad73d91256e2
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-09-22 13:56:12 -0300

    Setting up miragejs fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,33 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyle } from './styles/global';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          date: '21-09-2021',
+        },
+        {
+          id: 2,
+          title: 'transaction 2',
+          amount: 200,
+          type: 'withdrawl',
+          date: '21-09-2021',
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
