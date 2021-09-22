f97545d804f0b749615acd0220d5b2c496e93150
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-09-22 14:05:13 -0300

    Creating new transaction modal structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('root');
+
 function App() {
+  const [isNewTransactionModal, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Modal
+        isOpen={isNewTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
     </>
   );
